5559c879ef045efeec666b209df076c86f209343
Caio Azevedo <136472218+justCaioDev@users.noreply.github.com>
2024-09-23 15:47:52 -0300

    feat: add css

--- src/components/StatsPreviewCard/index.jsx
+++ src/components/StatsPreviewCard/index.jsx
@@ -1,14 +1,29 @@
+import CardContainer from './style'
+import DesktopImage from '/src/assets/image-header-desktop.jpg'
+import MobileImage from '/src/assets/image-header-mobile.jpg'
+
 export default function StatsPreviewCard() {
     return (
         <>
-            Get insights that help your business grow.
-
-            Discover the benefits of data analytics and make better decisions regarding revenue, customer 
-            experience, and overall efficiency.
-
-            10k+ companies
-            314 templates
-            12m+ queries
+            <CardContainer>
+                <div className="card-content">
+                    <h1 className="card-title">Get <span className="title-emphasis">insights</span> that help your business grow.</h1>
+                    <p className="card-description">Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
+                    </p>
+                    <div className="card-stats-container">
+                        <p className="stats"><span className="stats-emphasis">10k+</span> companies</p>
+                        <p className="stats"><span className="stats-emphasis">314</span> templates</p>
+                        <p className="stats"><span className="stats-emphasis">12m+</span> queries</p>
+                    </div>
+                </div>
+                <div className="card-img">
+                    <div className="bg-img"></div>
+                    <picture>
+                        <source media="(min-width: 1200px)" srcSet={DesktopImage} alt='A image to desktop' />
+                        <img src={MobileImage} alt="A image to mobile" />
+                    </picture>
+                </div>
+            </CardContainer>
         </>
     )
 }
--- src/components/StatsPreviewCard/style.js
+++ src/components/StatsPreviewCard/style.js
@@ -1,6 +1,84 @@
 import styled from "styled-components";
 
-const CardContainer = styled.div `
+const CardContainer = styled.section `
+    display: flex;
+
+    width: 100%;
+    max-width: 1150px;
+    border-radius: 8px;
+    overflow: hidden;
+    background-color:  hsl(244, 38%, 16%);
+
+    .card-content {
+        height: 100%;
+        
+        display: flex;
+        flex-direction: column;
+        padding: 64px;
+        padding-right: 80px;
+    }
+
+    .card-title {
+        font-size: 2.375rem;
+        font-weight: 700;
+        letter-spacing: 1px;
+        margin-bottom: 24px;
+        color: hsl(0, 0%, 100%);
+    }
+
+    .title-emphasis {
+        color: hsl(277, 64%, 61%);
+    }
+
+    .card-description {
+        font-weight: 400;
+        line-height: 1.5;
+        letter-spacing: 0.081rem;
+        padding-right: 58px;
+        margin-bottom: 80px;
+    }
+
+    .card-stats-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-right: 76px;
+    }
+
+    .stats {
+        font-family: 'Lexend Deca', sans-serif;
+        font-weight: 400;
+        text-transform: uppercase;
+        color: hsla(0, 0%, 100%, 0.6);
+    }
+
+    .stats-emphasis {
+        font-family: 'Inter', sans-serif;
+        font-size: 1.75rem;
+        font-weight: 700;
+        margin-bottom: 8px;
+        display: block;
+        color:  hsl(0, 0%, 100%);
+    }
+
+    .card-img {
+        position: relative;
+    }
+
+    .bg-img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #550985;
+        opacity: .6;
+    }
+    
+    .card-img img {
+        height: 100%;
+        
+    }
 
 
 `
